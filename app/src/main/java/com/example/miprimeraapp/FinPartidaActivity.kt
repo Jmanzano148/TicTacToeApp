bae9268e36538d9f9e7dd75a41d70ee2be30abc5
Jesús Manzano Álvarez <jma00068@red.ujaen.es>
2023-09-10 13:09:09 +0200

    Refactorizando lógica de control del juego

--- app/src/main/java/com/example/miprimeraapp/FinPartidaActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/FinPartidaActivity.kt
@@ -1,25 +1,16 @@
 package com.example.miprimeraapp
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
-import com.example.miprimeraapp.JuegoActivity.Companion.K_C11
-import com.example.miprimeraapp.JuegoActivity.Companion.K_C12
-import com.example.miprimeraapp.JuegoActivity.Companion.K_C13
-import com.example.miprimeraapp.JuegoActivity.Companion.K_C21
-import com.example.miprimeraapp.JuegoActivity.Companion.K_C22
-import com.example.miprimeraapp.JuegoActivity.Companion.K_C23
-import com.example.miprimeraapp.JuegoActivity.Companion.K_C31
-import com.example.miprimeraapp.JuegoActivity.Companion.K_C32
-import com.example.miprimeraapp.JuegoActivity.Companion.K_C33
 import com.example.miprimeraapp.JuegoActivity.Companion.K_NGANADOR
 import com.example.miprimeraapp.JuegoActivity.Companion.K_TGANADOR
-import com.example.miprimeraapp.JuegoActivity.Companion.K_VALORES_CASILLAS
+import com.example.miprimeraapp.JuegoActivity.Companion.NUM_COL
+import com.example.miprimeraapp.JuegoActivity.Companion.NUM_FILAS
+import com.example.miprimeraapp.JuegoActivity.Companion.tablero
 import com.example.miprimeraapp.MainActivity.Companion.K_NJUGADOR1
 import com.example.miprimeraapp.MainActivity.Companion.K_NJUGADOR2
 
@@ -33,17 +24,8 @@ class FinPartidaActivity : AppCompatActivity() {
     private lateinit var nJugador1: String // Nombre del jugador 1
     private lateinit var nJugador2: String // Nombre del jugador 2
     private lateinit var nombreGanador: String // Nombre del jugador ganador (null si hay empate)
-    private var turnoGanador: Int = 0 // Turno en el que se gano la partida (1 o 2)
 
-    private lateinit var c11: TextView // Casilla (1,1)
-    private lateinit var c12: TextView // Casilla (1,2)
-    private lateinit var c13: TextView // Casilla (1,3)
-    private lateinit var c21: TextView // Casilla (2,1)
-    private lateinit var c22: TextView // Casilla (2,2)
-    private lateinit var c23: TextView // Casilla (2,3)
-    private lateinit var c31: TextView // Casilla (3,1)
-    private lateinit var c32: TextView // Casilla (3,2)
-    private lateinit var c33: TextView // Casilla (3,3)
+    lateinit var tableroFinal: Array<Array<TextView>> // Almacena y visualiza el tablero final
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,20 +42,16 @@ class FinPartidaActivity : AppCompatActivity() {
         btnVolverInicio = findViewById(R.id.botonVolverInicio)
         btnVolverJugar = findViewById(R.id.botonVolverJugar)
 
-        turnoGanador = intent.extras?.getInt(K_TGANADOR) ?: 0
         nombreGanador = intent.extras?.getString(K_NGANADOR).toString()
         nJugador1 = intent.extras?.getString(K_NJUGADOR1).toString()
         nJugador2 = intent.extras?.getString(K_NJUGADOR2).toString()
 
-        c11 = findViewById(R.id.c11)
-        c12 = findViewById(R.id.c12)
-        c13 = findViewById(R.id.c13)
-        c21 = findViewById(R.id.c21)
-        c22 = findViewById(R.id.c22)
-        c23 = findViewById(R.id.c23)
-        c31 = findViewById(R.id.c31)
-        c32 = findViewById(R.id.c32)
-        c33 = findViewById(R.id.c33)
+        tableroFinal = Array(NUM_FILAS) { fila ->
+            Array(NUM_COL) { columna ->
+                findViewById(resources.getIdentifier("c${fila + 1}${columna + 1}", "id", packageName))
+            }
+        }
+
     }
 
     private fun initListeners() {
@@ -93,54 +71,19 @@ class FinPartidaActivity : AppCompatActivity() {
             imagenCentral.setImageResource(R.drawable.empate)
         } else { // Caso de victoria
             textoGanador.text =
-                "HA GANADO EL JUGADOR $turnoGanador *${nombreGanador?.toUpperCase()}*"
+                "HA GANADO EL JUGADOR *${nombreGanador?.toUpperCase()}*"
             imagenCentral.setImageResource(R.drawable.felicidades)
         }
         pintaTablero()
     }
 
     private fun pintaTablero() {
-        val sharedPreferences = getSharedPreferences(K_VALORES_CASILLAS, Context.MODE_PRIVATE)
-        c11.text = sharedPreferences.getString(K_C11, "")
-        c12.text = sharedPreferences.getString(K_C12, "")
-        c13.text = sharedPreferences.getString(K_C13, "")
-        c21.text = sharedPreferences.getString(K_C21, "")
-        c22.text = sharedPreferences.getString(K_C22, "")
-        c23.text = sharedPreferences.getString(K_C23, "")
-        c31.text = sharedPreferences.getString(K_C31, "")
-        c32.text = sharedPreferences.getString(K_C32, "")
-        c33.text = sharedPreferences.getString(K_C33, "")
-        if (nombreGanador != "null") buscaFilaGanadora()
-    }
-
-    private fun buscaFilaGanadora() {
-        // Comprueba filas horizontales
-        if (c11.text.toString() == c12.text.toString() && c11.text.toString() == c13.text.toString())
-            estableceFilaGanadora(c11, c12, c13)
-        if (c21.text.toString() == c22.text.toString() && c21.text.toString() == c23.text.toString())
-            estableceFilaGanadora(c21, c22, c23)
-        if (c31.text.toString() == c32.text.toString() && c31.text.toString() == c33.text.toString())
-            estableceFilaGanadora(c31, c32, c33)
-
-        // Comprueba filas verticales
-        if (c11.text.toString() == c21.text.toString() && c11.text.toString() == c31.text.toString())
-            estableceFilaGanadora(c11, c21, c31)
-        if (c12.text.toString() == c22.text.toString() && c12.text.toString() == c32.text.toString())
-            estableceFilaGanadora(c12, c22, c32)
-        if (c13.text.toString() == c23.text.toString() && c13.text.toString() == c33.text.toString())
-            estableceFilaGanadora(c13, c23, c33)
-
-        // Comprueba filas en diagonal
-        if (c11.text.toString() == c22.text.toString() && c11.text.toString() == c33.text.toString())
-            estableceFilaGanadora(c11, c22, c33)
-        if (c13.text.toString() == c22.text.toString() && c13.text.toString() == c31.text.toString())
-            estableceFilaGanadora(c13, c22, c31)
-    }
-
-    private fun estableceFilaGanadora(ficha1: TextView, ficha2: TextView, ficha3: TextView) {
-        ficha1.setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
-        ficha2.setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
-        ficha3.setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+        for (fila in tablero.indices) {
+            for (col in tablero[fila].indices) {
+                tableroFinal[fila][col].text = tablero[fila][col].text
+                tableroFinal[fila][col].setTextColor(tablero[fila][col].currentTextColor)
+            }
+        }
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/example/miprimeraapp/JuegoActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/JuegoActivity.kt
@@ -1,10 +1,10 @@
 package com.example.miprimeraapp
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.miprimeraapp.MainActivity.Companion.K_NJUGADOR1
 import com.example.miprimeraapp.MainActivity.Companion.K_NJUGADOR2
 
@@ -14,47 +14,26 @@ class JuegoActivity : AppCompatActivity() {
 
     companion object {
 
+        val NUM_FILAS = 3 // Número de filas del tablero
+        val NUM_COL = 3 // Número de columnas del tablero
+
         var FICHA1: String = "0"
         var FICHA2: String = "X"
 
+        lateinit var tablero: Array<Array<TextView>> // Almacena y visualiza el tablero
+
         const val K_NGANADOR: String = "NGANADOR"
         const val K_TGANADOR: String = "TGANADOR"
-        const val K_TABLERO_JUEGO: String = "TJUEGO"
-        const val K_VALORES_CASILLAS: String = "VCASILLAS"
-        const val K_C11: String = "C11"
-        const val K_C12: String = "C12"
-        const val K_C13: String = "C13"
-        const val K_C21: String = "C21"
-        const val K_C22: String = "C22"
-        const val K_C23: String = "C23"
-        const val K_C31: String = "C31"
-        const val K_C32: String = "C32"
-        const val K_C33: String = "C33"
     }
 
     private lateinit var tituloTurno: TextView // Muestra el turno del jugador
-    private lateinit var c11: TextView // Casilla (1,1)
-    private lateinit var c12: TextView // Casilla (1,2)
-    private lateinit var c13: TextView // Casilla (1,3)
-    private lateinit var c21: TextView // Casilla (2,1)
-    private lateinit var c22: TextView // Casilla (2,2)
-    private lateinit var c23: TextView // Casilla (2,3)
-    private lateinit var c31: TextView // Casilla (3,1)
-    private lateinit var c32: TextView // Casilla (3,2)
-    private lateinit var c33: TextView // Casilla (3,3)
 
     private lateinit var nJugador1: String // Nombre del jugador 1
     private lateinit var nJugador2: String // Nombre del jugador 2
 
-    private var turno: Int = 1 // Comienza el jugador 1
-    private var turnos: Int = 0 // Número de turnos jugados
-    private var finalizado: Boolean = false // Indica si ha terminado la partida
 
-    private var tablero: Array<Array<Int>> = arrayOf(
-        arrayOf(0, 0, 0),
-        arrayOf(0, 0, 0),
-        arrayOf(0, 0, 0)
-    )
+
+    private var fichaActual: String = FICHA1 // Almacena la ficha del turno actual
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,195 +47,119 @@ class JuegoActivity : AppCompatActivity() {
         nJugador1 = intent.extras?.getString(K_NJUGADOR1).toString()
         nJugador2 = intent.extras?.getString(K_NJUGADOR2).toString()
 
-        tituloTurno.text = "$TITULO_TURNO $turno $nJugador1"
+        tituloTurno.text = "$TITULO_TURNO $fichaActual $nJugador1"
     }
 
     private fun initComponents() {
         tituloTurno = findViewById(R.id.turnoJugador)
-        c11 = findViewById(R.id.c11)
-        c12 = findViewById(R.id.c12)
-        c13 = findViewById(R.id.c13)
-        c21 = findViewById(R.id.c21)
-        c22 = findViewById(R.id.c22)
-        c23 = findViewById(R.id.c23)
-        c31 = findViewById(R.id.c31)
-        c32 = findViewById(R.id.c32)
-        c33 = findViewById(R.id.c33)
+
+        tablero = Array(NUM_FILAS) { fila ->
+            Array(NUM_COL) { columna ->
+                findViewById(resources.getIdentifier("c${fila + 1}${columna + 1}", "id", packageName))
+            }
+        }
+
+        // Rellena cada TextView con espacios en blanco
+        for (fila in tablero.indices) {
+            for (columna in tablero[fila].indices) {
+                tablero[fila][columna].text = " "
+            }
+        }
+
     }
 
     private fun initListeners() {
-        c11.setOnClickListener {
-            if (tablero[0][0] == 0) {
-                tablero[0][0] = turno // Marco la casilla en el tablero
-                turnos++
-                c11.text = fichaSegunTurno()
-                compruebaFinPartida()
-                if (!finalizado) cambiaTurno()
-            }
-        }
-
-        c12.setOnClickListener {
-            if (tablero[0][1] == 0) {
-                tablero[0][1] = turno // Marco la casilla en el tablero
-                turnos++
-                c12.text = fichaSegunTurno()
-                compruebaFinPartida()
-                if (!finalizado) cambiaTurno()
-            }
-        }
-
-        c13.setOnClickListener {
-            if (tablero[0][2] == 0) {
-                tablero[0][2] = turno // Marco la casilla en el tablero
-                turnos++
-                c13.text = fichaSegunTurno()
-                compruebaFinPartida()
-                if (!finalizado) cambiaTurno()
-            }
-        }
-
-        c21.setOnClickListener {
-            if (tablero[1][0] == 0) {
-                tablero[1][0] = turno // Marco la casilla en el tablero
-                turnos++
-                c21.text = fichaSegunTurno()
-                compruebaFinPartida()
-                if (!finalizado) cambiaTurno()
-            }
-        }
-
-        c22.setOnClickListener {
-            if (tablero[1][1] == 0) {
-                tablero[1][1] = turno // Marco la casilla en el tablero
-                turnos++
-                c22.text = fichaSegunTurno()
-                compruebaFinPartida()
-                if (!finalizado) cambiaTurno()
-            }
-        }
-
-        c23.setOnClickListener {
-            if (tablero[1][2] == 0) {
-                tablero[1][2] = turno // Marco la casilla en el tablero
-                turnos++
-                c23.text = fichaSegunTurno()
-                compruebaFinPartida()
-                if (!finalizado) cambiaTurno()
-            }
-        }
-
-        c31.setOnClickListener {
-            if (tablero[2][0] == 0) {
-                tablero[2][0] = turno // Marco la casilla en el tablero
-                turnos++
-                c31.text = fichaSegunTurno()
-                compruebaFinPartida()
-                if (!finalizado) cambiaTurno()
-            }
-        }
-
-        c32.setOnClickListener {
-            if (tablero[2][1] == 0) {
-                tablero[2][1] = turno // Marco la casilla en el tablero
-                turnos++
-                c32.text = fichaSegunTurno()
-                compruebaFinPartida()
-                if (!finalizado) cambiaTurno()
-            }
-        }
-
-        c33.setOnClickListener {
-            if (tablero[2][2] == 0) {
-                tablero[2][2] = turno // Marco la casilla en el tablero
-                turnos++
-                c33.text = fichaSegunTurno()
-                compruebaFinPartida()
-                if (!finalizado) cambiaTurno()
+        for (fila in tablero.indices) {
+            for (col in tablero[fila].indices) {
+                tablero[fila][col].setOnClickListener {
+                    colocarFicha(fila, col)
+                    if(tableroLLeno() || compruebaVictoria())
+                        navegarFinPartida()
+                    else cambiarTurno()
+                }
             }
         }
     }
 
-    private fun cambiaTurno() {
+    private fun colocarFicha(fila: Int, col: Int) {
+        tablero[fila][col].text = fichaActual
+        tablero[fila][col].isClickable = false
+    }
+
+    private fun cambiarTurno() {
         var nombreJugador: String
-        if (turno == 1) {
-            turno = 2
+        if (fichaActual == FICHA1) {
+            fichaActual = FICHA2
             nombreJugador = nJugador2
         } else {
-            turno = 1
+            fichaActual = FICHA1
             nombreJugador = nJugador1
         }
-        tituloTurno.text = "$TITULO_TURNO $turno $nombreJugador"
+        tituloTurno.text = "$TITULO_TURNO $fichaActual $nombreJugador"
     }
 
-    private fun fichaSegunTurno(): String {
-        if (turno == 1) return FICHA1
-        return FICHA2
-    }
-
-    private fun fichaSegunPos(fila: Int, col: Int): String {
-        if (tablero[fila - 1][col - 1] == 1) return FICHA1
-        else if (tablero[fila - 1][col - 1] == 2) return FICHA2
-        return ""
-    }
-
-    private fun compruebaFinPartida() {
-        // Comprueba si ha ganado alguien
-        if ((tablero[0][0] == turno && tablero[0][1] == turno && tablero[0][2] == turno) ||
-            (tablero[1][0] == turno && tablero[1][1] == turno && tablero[1][2] == turno) ||
-            (tablero[2][0] == turno && tablero[2][1] == turno && tablero[2][2] == turno) ||
-            (tablero[0][0] == turno && tablero[1][0] == turno && tablero[2][0] == turno) ||
-            (tablero[0][1] == turno && tablero[1][1] == turno && tablero[2][1] == turno) ||
-            (tablero[0][2] == turno && tablero[1][2] == turno && tablero[2][2] == turno) ||
-            (tablero[0][0] == turno && tablero[1][1] == turno && tablero[2][2] == turno) ||
-            (tablero[0][2] == turno && tablero[1][1] == turno && tablero[2][0] == turno)
-        ) {
-            finalizado = true
-            navegarAGanador()
-        } else if (turnos == 9) { // Comprueba si hay un empate
-            finalizado = true
-            navegarAEmpate()
+    /**
+     * Función para verificar si el tablero está lleno (empate)
+     */
+    private fun tableroLLeno(): Boolean {
+        for (i in 0 until NUM_FILAS) {
+            for (j in 0 until NUM_COL) {
+                if (tablero[i][j].text == " ") {
+                    return false
+                }
+            }
         }
+        return true
     }
 
-    private fun navegarAGanador() {
-        var nombreGanador: String
+    /**
+     * Función para verificar si un jugador ha ganado
+     */
+    fun compruebaVictoria(): Boolean {
+        for (i in 0 until NUM_FILAS) {
+            if (tablero[i][0].text == fichaActual && tablero[i][1].text == fichaActual && tablero[i][2].text == fichaActual) {
+                tablero[i][0].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+                tablero[i][1].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+                tablero[i][2].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+                return true // Ganó en una fila
+            }
+            if (tablero[0][i].text == fichaActual && tablero[1][i].text == fichaActual && tablero[2][i].text == fichaActual) {
+                tablero[0][i].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+                tablero[1][i].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+                tablero[2][i].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+                return true // Ganó en una columna
+            }
+        }
+        if (tablero[0][0].text == fichaActual && tablero[1][1].text == fichaActual && tablero[2][2].text == fichaActual) {
+            tablero[0][0].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+            tablero[1][1].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+            tablero[2][2].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+            return true // Ganó en la diagonal principal
+        }
+        if (tablero[0][2].text == fichaActual && tablero[1][1].text == fichaActual && tablero[2][0].text == fichaActual) {
+            tablero[0][2].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+            tablero[1][1].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+            tablero[2][0].setTextColor(ContextCompat.getColor(this, R.color.fichaGanadora))
+            return true // Ganó en la diagonal secundaria
+        }
+        return false
+    }
 
-        if (turno == 1) nombreGanador = nJugador1
-        else nombreGanador = nJugador2
-
+    private fun navegarFinPartida() {
         var intent = Intent(this, FinPartidaActivity::class.java)
+        var nombreGanador: String? = null
+
+        // Si hay victoria
+        if (!tableroLLeno()) {
+            if (fichaActual == FICHA1) nombreGanador = nJugador1
+            else nombreGanador = nJugador2
+        }
+
+        intent.putExtra(K_NJUGADOR1, nJugador1)
+        intent.putExtra(K_NJUGADOR2, nJugador2)
         intent.putExtra(K_NGANADOR, nombreGanador)
-        intent.putExtra(K_TGANADOR, turno)
-        intent.putExtra(K_NJUGADOR1, nJugador1)
-        intent.putExtra(K_NJUGADOR2, nJugador2)
-        intent.putExtra(K_TABLERO_JUEGO, R.id.tableroJuego)
         startActivity(intent)
         finish()
-        guardaTablero()
     }
 
-    private fun navegarAEmpate() {
-        var intent = Intent(this, FinPartidaActivity::class.java)
-        intent.putExtra(K_NJUGADOR1, nJugador1)
-        intent.putExtra(K_NJUGADOR2, nJugador2)
-        intent.putExtra(K_TABLERO_JUEGO, R.id.tableroJuego)
-        startActivity(intent)
-        finish()
-        guardaTablero()
-    }
-
-    private fun guardaTablero() {
-        val sharedPreferences = getSharedPreferences(K_VALORES_CASILLAS, Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putString(K_C11, fichaSegunPos(1, 1))
-        editor.putString(K_C12, fichaSegunPos(1, 2))
-        editor.putString(K_C13, fichaSegunPos(1, 3))
-        editor.putString(K_C21, fichaSegunPos(2, 1))
-        editor.putString(K_C22, fichaSegunPos(2, 2))
-        editor.putString(K_C23, fichaSegunPos(2, 3))
-        editor.putString(K_C31, fichaSegunPos(3, 1))
-        editor.putString(K_C32, fichaSegunPos(3, 2))
-        editor.putString(K_C33, fichaSegunPos(3, 3))
-        editor.apply()
-    }
 }
